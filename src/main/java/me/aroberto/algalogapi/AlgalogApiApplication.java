525dbec766517624c3c6304eb7510cacd1f615c9
Alvaro Augusto Roberto <aroberto@unicamp.br>
2021-10-18 09:15:38 -0300

    Commit inicial.

---
+++ src/main/java/me/aroberto/algalogapi/AlgalogApiApplication.java
@@ -0,0 +1,13 @@
+package me.aroberto.algalogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgalogApiApplication.class, args);
+	}
+
+}
