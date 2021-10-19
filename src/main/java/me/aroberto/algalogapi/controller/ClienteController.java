3ad2b50e117c818ab2db1d313b86ffae820cb181
Alvaro Augusto Roberto <aroberto@unicamp.br>
2021-10-19 17:30:25 -0300

    Usando spring data jpa.

--- src/main/java/me/aroberto/algalogapi/controller/ClienteController.java
+++ src/main/java/me/aroberto/algalogapi/controller/ClienteController.java
@@ -1,30 +1,31 @@
 package me.aroberto.algalogapi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.aroberto.algalogapi.domain.model.Cliente;
+import me.aroberto.algalogapi.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 
+	private final ClienteRepository clienteRepository;
+
+	@Autowired
+	public ClienteController(ClienteRepository clienteRepository) {
+		super();
+		this.clienteRepository = clienteRepository;
+	}
+
+	public ClienteController() {
+		this(null);
+	}
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Alvaro Augusto Roberto");
-		cliente1.setEmail("alvaro.prog@gmail.com");
-		cliente1.setTelefone("(19) 98922-8225");
-
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Juliana Caroline Pertilli Roberto");
-		cliente2.setEmail("juliana.pertilli@gmail.com");
-		cliente2.setTelefone("(19) 99244-0844");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return clienteRepository.findAll();
 	}
 }
--- src/main/java/me/aroberto/algalogapi/domain/model/Cliente.java
+++ src/main/java/me/aroberto/algalogapi/domain/model/Cliente.java
@@ -1,7 +1,17 @@
 package me.aroberto.algalogapi.domain.model;
 
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
@@ -37,5 +47,22 @@ public class Cliente {
 
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
 	}
 }
