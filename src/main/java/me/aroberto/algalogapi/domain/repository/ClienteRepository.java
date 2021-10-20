06679e2493904dfc9a0032168273b2c49c65e14e
Alvaro Augusto Roberto <aroberto@unicamp.br>
2021-10-20 08:11:32 -0300

    Implementação das funcionalidades de CRUD do recurso Cliente..

--- src/main/java/me/aroberto/algalogapi/domain/repository/ClienteRepository.java
+++ src/main/java/me/aroberto/algalogapi/domain/repository/ClienteRepository.java
@@ -1,6 +1,7 @@
 package me.aroberto.algalogapi.domain.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,5 @@ import me.aroberto.algalogapi.domain.model.Cliente;
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
 	List<Cliente> findByNome(String nome);
+	Optional<Cliente> findById(Long id);
 }
