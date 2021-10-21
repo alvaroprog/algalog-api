606eef27f8f0baed26e2c566656e3f885aa08b5b
Alvaro Augusto Roberto <aroberto@unicamp.br>
2021-10-21 07:51:57 -0300

    Iniciando implementação de beans validation e exception handler.

--- src/main/java/me/aroberto/algalogapi/domain/model/Cliente.java
+++ src/main/java/me/aroberto/algalogapi/domain/model/Cliente.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Cliente {
@@ -13,8 +16,18 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@NotEmpty
+	@Size(max = 60)
 	private String nome;
+
+	@NotEmpty
+	@Email
+	@Size(max = 255)
 	private String email;
+
+	@NotEmpty
+	@Size(max = 20)
 	private String telefone;
 
 	public Long getId() {
